626ad56d62fc7a943bcda437ea7e96e9dc372714
snowmeili <545442742@qq.com>
2021-07-26 23:03:21 +0800

    fix: ceshi

--- src/app.tsx
+++ src/app.tsx
@@ -7,6 +7,7 @@ import renderRoutes from "./utils/renderRouter";
 export default function App(): JSX.Element {
   const authed = false; // 如果登陆之后可以利用redux修改该值(关于redux不在我们这篇文章的讨论范围之内）
   const authPath = "/login"; // 默认未登录的时候返回的页面，可以自行设置
+  console.log(111)
 
   return (
     <BrowserRouter>
