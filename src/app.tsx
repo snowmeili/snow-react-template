f4bd1f7a7f43a4fe879134aef41f9078f1506f7a
snowmeili <545442742@qq.com>
2021-07-27 10:20:51 +0800

    fix: 修改 prettier

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { BrowserRouter, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
-import routes from "./router/index";
-import renderRoutes from "./utils/renderRouter";
+import routes from './router/index';
+import renderRoutes from './utils/renderRouter';
 
 export default function App(): JSX.Element {
   const authed = false; // 如果登陆之后可以利用redux修改该值(关于redux不在我们这篇文章的讨论范围之内）
-  const authPath = "/login"; // 默认未登录的时候返回的页面，可以自行设置
-  console.log(111)
+  const authPath = '/login'; // 默认未登录的时候返回的页面，可以自行设置
+  console.log(111);
 
   return (
     <BrowserRouter>
